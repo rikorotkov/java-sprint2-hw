20da508d3ea4f6e26f108f5e28430225ce6fe930
Roman <floda935@gmail.com>
2024-09-23 22:19:31 +0300

    create menu

---
+++ src/ru/rikorotkov/Main.java
@@ -0,0 +1,41 @@
+package ru.rikorotkov;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            printMenu();
+
+            try {
+                String command = scanner.next();
+
+                if (Integer.parseInt(command) == 1) {
+                    System.out.println("Выполняется команда 1");
+                } else if (Integer.parseInt(command) == 2) {
+                    System.out.println("Выполняется команда 2");
+                } else if (Integer.parseInt(command) == 3) {
+                    System.out.println("Выполняется команда 3");
+                } else if (Integer.parseInt(command) == 4) {
+                    return;
+                } else {
+                    System.out.println("Неверная команда.");
+                }
+            } catch (Exception e) {
+                System.out.println("Неверная команда.");
+            }
+
+        }
+
+    }
+
+    static void printMenu() {
+        System.out.println("Выберите команду:");
+        System.out.println("1. Ввести количество шагов за определенный день.");
+        System.out.println("2. Изменить цель по количеству шагов за день.");
+        System.out.println("3. Напечатать статистику за определенный месяц.");
+        System.out.println("4. Выйти из приложения.");
+    }
+}
